62247dec1767aca4261d644fa03c850f81257fd5
NikSQU <nicolo.vernetti@gmail.com>
2026-08-31 16:56:20 +0200

    refactoring

--- scene.hh
+++ scene.hh
@@ -5,12 +5,14 @@
 #include "camera.hh"
 #include "lights.hh"
 #include "gpumesh.hh"
+#include <string>
 
 // Forward declaration
 namespace fcg { class Shaders; }
 
 class Scene {
 public:
+    Scene(std::string meshfile, fcg::Shaders& shaders);
     float velocita_rotore = 0.5f;
     float velocita_salvata = 0.5f;
     float angolo_rotore = 0.0f;
